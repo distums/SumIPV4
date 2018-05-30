1e659f9f44f5518d04a5f9f8837721579d973ffa
tumingsheng928 <tumingsheng928@lu.com>
2018-05-30 14:19:56 +0800

    sum ipv4 address

--- src/sum.js
+++ src/sum.js
@@ -1,33 +1,72 @@
+const assert = require('assert');
+
 function sum(ip) {
-  let total = '';
+  assert.strictEqual(typeof ip, 'string', 'invalid arguments');
+  const parts = parse(ip);
+  return parseInt(parts.map(toByte).join(''), '2');
+}
+
+/**
+ * parse an ip address string to an array with four elements
+ * eg. '172.168.5.1' => [172,168,5,1]
+ * @param {string} ip ip address string
+ * @returns {Array}
+ */
+function parse(ip) {
+  let parts = [];
   let currentSeg = '';
+  let containsSpaceAfterDigit = false;
   for (const char of ip) {
-    if (char === '.') {
-      total += toByte(currentSeg);
-      currentSeg = '';
-    } else {
+    if (char >= '0' && char <= '9') {
+      if (containsSpaceAfterDigit)
+        throw new Error('spaces between two digits is invalid');
       currentSeg += char;
+    } else if (char === '.') {
+      parts.push(convertToNumber(currentSeg));
+      currentSeg = '';
+      containsSpaceAfterDigit = false;
+    } else if (char === ' ') {
+      if (currentSeg.length > 0) {
+        containsSpaceAfterDigit = true;
+      }
+    } else {
+      throw new Error('ip address string contains invalid character');
     }
   }
-  total += toByte(currentSeg);
-  return parseInt(total, 2);
+  if (currentSeg) {
+    parts.push(convertToNumber(currentSeg));
+  }
+  if (parts.length !== 4) {
+    throw new Error('invalid ip address');
+  }
+  return parts;
 }
 
-function toByte(str) {
-  return parseInt(str, 10)
-    .toString(2)
-    .padStart(8, '0');
+function toByte(num) {
+  return toBinary(num).padStart(8, '0');
 }
 
+function convertToNumber(part) {
+  const num = parseInt(part, 10);
+  if (isNaN(num) || num > 255) throw new Error('invalid ip address');
+  return num;
+}
+
+/**
+ * convert decimal to binary
+ * @param {number} num decimal number
+ * @param {array} arr temp array
+ * @returns {string} binary representation of input num
+ */
 function toBinary(num, arr = []) {
   if (num < 2) {
     arr.push(num);
-    return arr.reverse.join('');
+    return arr.reverse().join('');
   }
-  const devider = num / 2;
-  const remain = num % 2;
-  arr.push(remain);
-  return toBinary(num, arr);
+  const quotient = Math.floor(num / 2);
+  const remainder = num % 2;
+  arr.push(remainder);
+  return toBinary(quotient, arr);
 }
 
 module.exports = sum;
--- test/sumIPV4.test.js
+++ test/sumIPV4.test.js
@@ -20,14 +20,77 @@ test('sum ` 172  . 168.5 .1 ` which contains valid spaces to equal `2896692481` 
   expect(binaryStr.slice(24)).toBe('00000001'); // lowest order 8 bit
 });
 
-// test('sum `17 2.168.5.1 ` which contains spaces between two digits should throw error', () => {
-//   expect(sum('17 2.168.5.1')).toThrow();
-// });
+test('sum `172.16 8.5.1 ` which contains spaces between two digits should throw error', () => {
+  expect(() => {
+    sum('172.16 8.5.1');
+  }).toThrow('spaces between two digits');
+});
 
-// test('sum an invalid ip address `172.168.5` should throw error', () => {
-//   expect(sum('172.168.5')).toThrow();
-// });
+test('sum an invalid ip address `172.168.5` should throw error', () => {
+  expect(() => {
+    sum('172.168.5');
+  }).toThrow('invalid ip address');
+});
 
-// test('sum an invalid ip address `256.168.5.1` should throw error', () => {
-//   expect(sum('17 2.168.5.1')).toThrow();
-// });
+test('sum an invalid ip address `.172.168.5` should throw error', () => {
+  expect(() => {
+    sum('.172.168.5.');
+  }).toThrow('invalid ip address');
+});
+
+test('sum an invalid ip address `256.168.5.1` should throw error', () => {
+  expect(() => {
+    sum('256.168.5.1');
+  }).toThrow('invalid ip address');
+});
+
+test('sum an invalid ip address `178.-168.5.1` should throw error', () => {
+  expect(() => {
+    sum('178.-168.5.1');
+  }).toThrow('contains invalid character');
+});
+
+test('sum an invalid ip address `178` should throw error', () => {
+  expect(() => {
+    sum('178');
+  }).toThrow('invalid ip address');
+});
+
+test('providing invalid arguments to `sum` should throw error', () => {
+  expect(() => {
+    sum();
+  }).toThrow('invalid arguments');
+  expect(() => {
+    sum(null);
+  }).toThrow('invalid arguments');
+  expect(() => {
+    sum(172);
+  }).toThrow('invalid arguments');
+  expect(() => {
+    sum({});
+  }).toThrow('invalid arguments');
+});
+
+test('sum random generated ip string should have correct result', () => {
+  const ips = [
+    randomByteNum(),
+    randomByteNum(),
+    randomByteNum(),
+    randomByteNum(),
+  ];
+  const ip = ips.join('.');
+  const result = sum(ip);
+  expect(result).toBe(
+    ips[0] * 2 ** 24 + ips[1] * 2 ** 16 + ips[2] * 2 ** 8 + ips[3]
+  );
+  const binaryStr = result.toString(2).padStart(32, '0');
+  expect(binaryStr.slice(0, 8)).toBe(ips[0].toString(2).padStart(8, '0')); // highest order 8 bit
+  expect(binaryStr.slice(8, 16)).toBe(ips[1].toString(2).padStart(8, '0')); // second highest order 8 bit
+  expect(binaryStr.slice(16, 24)).toBe(ips[2].toString(2).padStart(8, '0')); // second lowest order 8 bit
+  expect(binaryStr.slice(24)).toBe(ips[3].toString(2).padStart(8, '0')); // lowest order 8 bit
+});
+
+function randomByteNum() {
+  const r = Math.random();
+  return Math.floor(r * 256);
+}
